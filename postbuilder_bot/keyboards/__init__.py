3e00bae0d7ba560b9a566cd9b1c4e0d140d1a860
ibnmurtaza4-web <ibnmurtaza4@gmail.com>
2026-06-15 11:24:56 +0400

    Add postbuilder_bot/keyboards/__init__.py

---
+++ postbuilder_bot/keyboards/__init__.py
@@ -0,0 +1,24 @@
+"""Klaviatura paketi"""
+from .keyboards import (
+    main_menu_keyboard, post_builder_keyboard,
+    text_format_keyboard, media_type_keyboard,
+    button_type_keyboard, button_menu_keyboard,
+    buttons_list_keyboard, send_target_keyboard,
+    confirm_keyboard, back_keyboard, back_cancel_keyboard,
+    preview_keyboard, drafts_keyboard, templates_keyboard,
+    json_mode_keyboard, premium_menu_keyboard,
+    admin_keyboard, build_post_inline_keyboard,
+    options_keyboard,
+)
+
+__all__ = [
+    "main_menu_keyboard", "post_builder_keyboard",
+    "text_format_keyboard", "media_type_keyboard",
+    "button_type_keyboard", "button_menu_keyboard",
+    "buttons_list_keyboard", "send_target_keyboard",
+    "confirm_keyboard", "back_keyboard", "back_cancel_keyboard",
+    "preview_keyboard", "drafts_keyboard", "templates_keyboard",
+    "json_mode_keyboard", "premium_menu_keyboard",
+    "admin_keyboard", "build_post_inline_keyboard",
+    "options_keyboard",
+]
