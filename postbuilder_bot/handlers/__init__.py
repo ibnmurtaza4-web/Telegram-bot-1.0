1f46b50b02a7d7510c727a2a2ff3779f26895fc2
ibnmurtaza4-web <ibnmurtaza4@gmail.com>
2026-06-15 11:24:43 +0400

    Add postbuilder_bot/handlers/__init__.py

---
+++ postbuilder_bot/handlers/__init__.py
@@ -0,0 +1,19 @@
+"""Handlers paketi"""
+from . import (
+    start,
+    post_builder,
+    button_builder,
+    media_handler,
+    templates_handler,
+    send_handler,
+    admin_handler,
+    json_handler,
+    premium_handler,
+    drafts_handler,
+)
+
+__all__ = [
+    "start", "post_builder", "button_builder", "media_handler",
+    "templates_handler", "send_handler", "admin_handler",
+    "json_handler", "premium_handler", "drafts_handler",
+]
